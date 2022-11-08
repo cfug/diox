6354afcd8aa1784e05ee61b795cff1ab7c58811c
Alex Li <github@alexv525.com>
2022-11-08 21:23:37 +0800

    ++

--- dio/test/request_test.dart
+++ dio/test/request_test.dart
@@ -86,14 +86,20 @@ void main() {
       assert(ri.method == 'GET');
     });
 
-
     test('#test multi value headers', () async {
       Response response = await dio.get(
         '/multi-value-header',
-         options: Options(headers: {'x-multi-value-request-header': ['value1', 'value2']})
+        options: Options(
+          headers: {
+            'x-multi-value-request-header': ['value1', 'value2'],
+          },
+        ),
       );
       expect(response.statusCode, 200);
-      expect(response.headers.value("x-multi-value-request-header-echo"), equals('value1, value2'));
+      expect(
+        response.headers.value('x-multi-value-request-header-echo'),
+        equals('value1, value2'),
+      );
     });
 
     test('#test request with URI', () async {
--- dio/test/utils.dart
+++ dio/test/utils.dart
@@ -76,7 +76,10 @@ Future<void> startServer() async {
 
       if (path == '/multi-value-header') {
         response.headers.contentType = ContentType('application', 'json');
-        response.headers.add("x-multi-value-request-header-echo", request.headers.value("x-multi-value-request-header").toString());
+        response.headers.add(
+          'x-multi-value-request-header-echo',
+          request.headers.value('x-multi-value-request-header').toString(),
+        );
         response
           ..statusCode = 200
           ..contentLength = -1
