d60fddb00bbde41e6f821e16549555af5a6274cb
Alex Li <github@alexv525.com>
2022-11-15 16:44:44 +0800

    Fix tests on GitHub

--- test.sh
+++ test.sh
@@ -1,12 +1,4 @@
-openssl s_client \
-  -servername badssl.com \
-  -connect badssl.com:443 < /dev/null 2>/dev/null \
-  | openssl x509 -noout -fingerprint -sha256 > dio/test/_pinning.txt 2>/dev/null
-openssl s_client \
-  -servername httpbin.org \
-  -connect httpbin.org:443 < /dev/null 2>/dev/null \
-  | openssl x509 -noout -fingerprint -sha256 > plugins/http2_adapter/test/_http2_pinning.txt 2>/dev/null
-cd dio
+cd dio || exit
 dart test --coverage=coverage .
 pub run coverage:format_coverage --packages=.packages -i coverage -o coverage/lcov.info --lcov
 genhtml -o coverage coverage/lcov.info
